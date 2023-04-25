17cd4c09df9dff5f198ff8be5d690400db9fbda8
papangkorn <butter0026@gmail.com>
2023-04-25 11:46:54 +0700

    add Lab5

---
+++ src/main/java/byteStream/Student.java
@@ -0,0 +1,16 @@
+package byteStream;
+
+import java.io.Serializable;
+
+public class Student implements Serializable {
+    String id;
+    String name;
+
+    public Student(String id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    
+    
+}
